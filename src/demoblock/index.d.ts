12a3952bc6ae347c54a3bb74ac9b2f479b5b1f09
xiaoxustudio <1783558957@qq.com>
2024-08-28 09:11:01 +0800

    update d.ts

--- src/demoblock/index.d.ts
+++ src/demoblock/index.d.ts
@@ -1,9 +1,13 @@
-declare module '*.vue' {
-    const component: import("vue").DefineComponent<{}, {}, any>
-    export default component
+declare module "*.vue" {
+	import type { DefineComponent } from "vue";
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
 }
 
-declare module 'vitepress-demoblock' {
-    const demoblock: (md: any) => void;
-    export default demoblock
+declare module "vitepress-demoblock-xr" {
+	const demoblock: (md: any) => void;
+	import type { DefineComponent } from "vue";
+	const demo: DefineComponent<{}, {}, any>;
+	export default demoblock;
 }
